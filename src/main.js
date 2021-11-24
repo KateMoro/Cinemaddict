c16c813490fbc15d5aa55bcd861aa5024706f6eb
Katerina <kathty@gmail.com>
2021-11-24 03:48:39 +0300

    Разбивает вёрстку на отдельные компоненты

---
+++ src/main.js
@@ -0,0 +1,39 @@
+import { renderTemplate, RenderPosition } from './render.js';
+import { createProfileTemplate } from './view/profile-view.js';
+import { createMainNavigationTemplate } from './view/main-navigation-view.js';
+import { createSortTemplate } from './view/sort-view.js';
+import { createFilmsTemplate } from './view/films-view.js';
+import { createFilmCardTemplate } from './view/film-card-view.js';
+import { createLoadMoreButtonTemplate } from './view/load-more-button-view.js';
+import { createFooterStatisticsTemplate } from './view/footer-statistics-view.js';
+import { createFilmDetailsPopupTemplate } from './view/film-details-popup-view.js';
+
+const CARD_COUNT = 5;
+const EXTRA_CARD_COUNT = 2;
+
+const pageHeader = document.querySelector('.header');
+const pageMain = document.querySelector('.main');
+const pageFooter = document.querySelector('.footer');
+const pageFooterStatistics = pageFooter.querySelector('.footer__statistics');
+
+renderTemplate(pageHeader, createProfileTemplate(), RenderPosition.BEFOREEND);
+renderTemplate(pageMain, createMainNavigationTemplate(), RenderPosition.BEFOREEND);
+renderTemplate(pageMain, createSortTemplate(), RenderPosition.BEFOREEND);
+renderTemplate(pageMain, createFilmsTemplate(), RenderPosition.BEFOREEND);
+renderTemplate(pageFooterStatistics, createFooterStatisticsTemplate(), RenderPosition.BEFOREEND);
+renderTemplate(pageFooter, createFilmDetailsPopupTemplate(), RenderPosition.AFTEREND);
+
+const filmList = pageMain.querySelector('.films-list__container');
+const filmListExtra = pageMain.querySelectorAll('.films-list--extra .films-list__container');
+
+for (let i = 0; i < CARD_COUNT; i++) {
+  renderTemplate(filmList, createFilmCardTemplate(), RenderPosition.BEFOREEND);
+}
+
+renderTemplate(filmList, createLoadMoreButtonTemplate(), RenderPosition.BEFOREEND);
+
+filmListExtra.forEach( (list) => {
+  for (let i = 0; i < EXTRA_CARD_COUNT; i++) {
+    renderTemplate(list, createFilmCardTemplate(), RenderPosition.BEFOREEND);
+  }
+});
---
+++ src/view/load-more-button-view.js
@@ -0,0 +1,3 @@
+export const createLoadMoreButtonTemplate = () => (`
+  <button class="films-list__show-more">Show more</button>
+`);
