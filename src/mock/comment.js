c8f5eb19b62222bfd3f0814dccadbe9d33b20520
Katerina <kathty@gmail.com>
2021-11-29 23:48:13 +0300

    Добавляет моковые данные

---
+++ src/mock/comment.js
@@ -0,0 +1,40 @@
+import dayjs from 'dayjs';
+import { getRandomNumber, getRandomArrayElement } from './../utils.js';
+
+const authors = [
+  'Kuzma Sharp',
+  'Andzelika Franke',
+  'Klara Darcy',
+  'Red Alfaro',
+  'Ernest Snell',
+];
+
+const comments = [
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  'Cras aliquet varius magna, non porta ligula feugiat eget.',
+  'Fusce tristique felis at fermentum pharetra.',
+  'Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante.',
+];
+
+const emotions = [
+  'smile',
+  'sleeping',
+  'puke',
+  'angry',
+];
+
+const generateCommentDate = () => {
+  const maxDaysGap = 90;
+  const daysGap = getRandomNumber(-maxDaysGap, 0);
+
+  return dayjs().add(daysGap, 'day');
+};
+
+const generateComment = () => ({
+  author: getRandomArrayElement(authors),
+  comment: getRandomArrayElement(comments),
+  date: generateCommentDate(),
+  emotion: getRandomArrayElement(emotions),
+});
+
+export { generateComment };
