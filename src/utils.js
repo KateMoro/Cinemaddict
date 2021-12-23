447183b4892454827b9564d0e6f76064e06dd1cd
Katerina <kathty@gmail.com>
2021-12-23 18:51:11 +0300

    Добавляет вспомогательную функцию updateItem

--- src/utils.js
+++ src/utils.js
@@ -31,4 +31,18 @@ const getTimeFromMins = (mins) => {
 
 const getFormattedDate = (date, format) => dayjs(date).format(format);
 
-export {getRandomNumber, getRandomArrayElement, getRandomArray, getTimeFromMins, getFormattedDate};
+const updateItem = (items, update) => {
+  const index = items.findIndex((item) => item.id === update.id);
+
+  if (index === -1) {
+    return items;
+  }
+
+  return [
+    ...items.slice(0, index),
+    update,
+    ...items.slice(index + 1),
+  ];
+};
+
+export {getRandomNumber, getRandomArrayElement, getRandomArray, getTimeFromMins, getFormattedDate, updateItem};
