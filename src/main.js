3e3ec917213ba143c7d61b63dab43d6db7044152
Katerina <kathty@gmail.com>
2022-01-31 13:17:32 +0300

    Удаляет моки

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { generateFilmCard } from './mock/film-card.js';
+
 import { render } from './utils/render.js';
 
 import ProfileView from './view/profile-view.js';
@@ -7,9 +7,8 @@ import FooterStatisticsView from './view/footer-statistics-view.js';
 
 import FilmsListPresenter from './presenter/films-list-presenter.js';
 
-const CARD_COUNT = 18;
 
-const cards = Array.from({length: CARD_COUNT}, generateFilmCard);
+
 
 const pageHeader = document.querySelector('.header');
 const pageMain = document.querySelector('.main');
--- src/utils/common.js
+++ src/utils/common.js
@@ -1,48 +1,11 @@
 import dayjs from 'dayjs';
 
-const getRandomNumber = (a, b, precision) => {
-  const min = Math.ceil(Math.min(a, b));
-  const max = Math.floor(Math.max(a, b));
-
-  return Number((Math.random() * (max - min) + min).toFixed(precision));
-};
-
-const getRandomArrayElement = (elements) => elements[getRandomNumber(0, elements.length - 1)];
-
-const getRandomArray = (arr) => {
-  const rand = getRandomNumber(1, arr.length);
-  const newArr = [];
-  while (newArr.length !== rand) {
-    const elem = arr[getRandomNumber(0, arr.length-1)];
-    if (newArr.indexOf(elem) === -1) {
-      newArr.push(elem);
-    }
-  }
-
-  return newArr;
-};
-
-const getTimeFromMins = (mins) => {
+export const getTimeFromMins = (mins) => {
   const hours = Math.trunc(mins / 60);
   const minutes = mins % 60;
 
   return hours ? `${hours}h ${minutes}m` : `${minutes}m`;
 };
 
-const getFormattedDate = (date, format) => dayjs(date).format(format);
+export const getFormattedDate = (date, format) => dayjs(date).format(format);
 
-const updateItem = (items, update) => {
-  const index = items.findIndex((item) => item.id === update.id);
-
-  if (index === -1) {
-    return items;
-  }
-
-  return [
-    ...items.slice(0, index),
-    update,
-    ...items.slice(index + 1),
-  ];
-};
-
-export {getRandomNumber, getRandomArrayElement, getRandomArray, getTimeFromMins, getFormattedDate, updateItem};
